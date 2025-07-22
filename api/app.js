1af0cc7a7f02df213502970db1f218177984bd41
Aryasuta CM <aryasutamadana@gmail.com>
2025-07-22 23:59:53 +0700

    api update

--- api/app.js
+++ api/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'https://sehat.teluapp.org/',
+  origin: 'https://sehat.teluapp.org',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
