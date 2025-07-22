437fefaa4a49993e6b32da7f61b470591526beea
Aryasuta CM <aryasutamadana@gmail.com>
2025-07-22 17:11:45 +0700

    api update

--- api/app.js
+++ api/app.js
@@ -39,7 +39,7 @@ app.use('/users', userRoutes);
 app.use('/sessions', sessionRoutes);
 app.use('/scan', scanRoutes);
 
-const PORT = process.env.PORT || 3210;
+const PORT = process.env.PORT || 3000;
 // app.listen(PORT, '0.0.0.0', () => {
 //   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 // });
