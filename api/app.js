315d2cb410554038a6f7a442cf40f1a7abe7e1a6
Aryasuta CM <aryasutamadana@gmail.com>
2025-07-22 15:25:27 +0700

    api update

--- api/app.js
+++ api/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const { connectDB } = require(__dirname + '/config/db');
+const { connectDB } = require('../config/db');
 
-const userRoutes = require(__dirname + '/routes/userRoutes');
-const sessionRoutes = require(__dirname + '/routes/sessionRoutes');
-const scanRoutes = require(__dirname + '/routes/scanRoutes');
+const userRoutes = require('../routes/userRoutes');
+const sessionRoutes = require('../routes/sessionRoutes');
+const scanRoutes = require('../routes/scanRoutes');
 
 dotenv.config();
 connectDB();
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
 
 app.use(cors({
   origin: '*',
+  //origin: 'https://monitoringsepedateluapp.my.id', // Pastikan ini benar
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
