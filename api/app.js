9361b92f57873ff413cdee70f465e66bb21ea8a7
Aryasuta CM <aryasutamadana@gmail.com>
2025-07-22 16:07:24 +0700

    api update

--- api/app.js
+++ api/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://sehat.teluapp.org',
   //origin: 'https://monitoringsepedateluapp.my.id', // Pastikan ini benar
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
