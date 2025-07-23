02bfeb69e4004a4e98ef6e8e91e4fb9134b3b637
Aryasuta CM <aryasutamadana@gmail.com>
2025-07-23 17:23:01 +0700

    api restore

--- api/app.js
+++ api/app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const { connectDB } = require(__dirname + '/config/db');
+const { connectDB } = require('../config/db');
 
-const userRoutes = require(__dirname + '/routes/userRoutes');
-const sessionRoutes = require(__dirname + '/routes/sessionRoutes');
-const scanRoutes = require(__dirname + '/routes/scanRoutes');
+const userRoutes = require('../routes/userRoutes');
+const sessionRoutes = require('../routes/sessionRoutes');
+const scanRoutes = require('../routes/scanRoutes');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-const path = require('path');
 
 // ✅ Serving static files (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, '../frontend/public')));  // ✅ Tambahkan ini
+app.use(express.static('public'));  // ✅ Tambahkan ini
 
 app.use(express.json());
 
@@ -29,8 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: '*',
-  //origin: 'https://monitoringsepedateluapp.my.id', // Pastikan ini benar
+  origin: 'https://sehat.teluapp.org',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
@@ -41,9 +39,9 @@ app.use('/users', userRoutes);
 app.use('/sessions', sessionRoutes);
 app.use('/scan', scanRoutes);
 
-const PORT = process.env.PORT || 3210;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
+// });
 
 module.exports = app;
